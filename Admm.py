22cb1c242d11b11158c8cc6ce464067fad351944
hivergX36 <vic.herve44@gmail.com>
2026-05-10 14:40:42 +0200

    2026_05_10_start_admm

--- Admm.py
+++ Admm.py
@@ -1,5 +1,1 @@
-class Admm:
-    def __init__(self, epsilon, rho, max_iter, A, b, C, d):
-        self.epsilon = epsilon
-        self.rho = rho
-        self.max_iter = max_iter
+
--- Main.py
+++ Main.py
@@ -6,6 +6,13 @@ from Abstract_player_modelling_type_2 import Player_abstract_model_type_2
 from Agent_class_controler import controler
 
 # Sample data for players, prices, and revenues
+NUMBER_ITER = 50
+Epsilon = 10 ^ -6
+
+# Admm criteria
+step = 0.01
+eps = 1
+num_iter = 0
 
 # Create and run the model
 game_controller = controler(number_of_agent_type_1=10, number_of_agent_type_2=10)
@@ -14,11 +21,19 @@ game_controller.read_data_player_type_2("data_player_type_2.txt")
 game_controller.read_data_aggregator("data_aggregator.txt")
 
 # Print agent programs by the controler
-game_controller.print_controler()
-game_controller.make_instance()
-game_controller.print_instance()
 
 model_type_1 = Player_abstract_model_type_1()
-print(game_controller.agent_list_type_1[0].instance)
-model_type_1.run_model(game_controller.agent_list_type_1[0].instance)
-model_type_1.run_model(game_controller.agent_list_type_1[1].instance)
+model_type_2 = Player_abstract_model_type_2()
+
+while eps > Epsilon and num_iter < NUMBER_ITER:
+    for agent in game_controller.agent_list_type_1:
+        agent.make_instance()
+        model_type_1.run_model(agent.instance)
+    for agent in game_controller.agent_list_type_2:
+        agent.make_instance()
+        model_type_2.run_model(agent.instance)
+    for 
+    game_controller.aggregator.make_instance(
+        game_controller.agent_list_type_1, game_controller.agent_list_type_2
+    )
+    game_controller.aggregator.run_model()
