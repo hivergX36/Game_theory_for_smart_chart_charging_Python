e47bf43f348ab805c6973b2e20bf563fe4f44f7a
Victor <115408919+hivergX36@users.noreply.github.com>
2026-05-16 19:12:27 +0200

    Update_agent_class_modeling_type_2_2026_05_16

--- Abstract_player_modelling_type_2.py
+++ Abstract_player_modelling_type_2.py
@@ -6,38 +6,33 @@ class Player_abstract_model_type_2:
     def __init__(self):
         self.player_solution = 0
         self.model = AbstractModel()
-        self.model.lambda_p = Param(within=NonNegativeReals)
+        self.model.lambda_price_2 = Param(within=NonNegativeReals)
         self.model.r = Param(within=NonNegativeReals)
         self.model.Q = Param(within=NonNegativeReals)
+        self.model.Q2 = Param(within=NonNegativeReals)
         self.model.sum_another_demand_type_1 = Param(within=NonNegativeReals)
         self.model.sum_another_demand_type_2 = Param(within=NonNegativeReals)
         self.model.t = Param(within=NonNegativeReals)
         self.model.max_demand = Param(within=NonNegativeReals)
-        self.model.waiting_price = Param(within=NonNegativeReals)
+        self.model.waiting_price = Param(within=Reals)
 
         # Define the decision variables for demand, indexed by players
-        self.model.d1 = Var(within=NonNegativeReals)
+        self.model.d2 = Var(within=NonNegativeReals)
 
         # Define the objective: maximize total demand across all players
         def objective_rule(model):
-            return model.d1 - model.waiting_price * (
-                model.d1
-                + model.sum_another_demand_type_1
-                + model.sum_another_demand_type_2
-                + 1 / model.t
-                - model.Q
-            )
+            return model.d2
 
         self.model.objective = Objective(rule=objective_rule, sense=maximize)
 
         # Define the revenue constraint: demand * price should be less than or equal to revenue
         def revenue_rule(model, p):
-            return model.d1 * model.lambda_p <= model.r
+            return model.d2 * model.lambda_price_2 <= model.r
 
         self.model.revenue_constraint = Constraint(rule=revenue_rule)
 
         def model_demand_constraint(model):
-            return model.d1 <= model.max_demand
+            return model.d2 <= model.max_demand
 
         self.model.demand_constraint = Constraint(rule=model_demand_constraint)
 
@@ -49,5 +44,5 @@ class Player_abstract_model_type_2:
         # Solve the model
         solver.solve(instance)
         print("Results for Player 2:")
-        instance.d1.pprint()
-        self.player_solution = instance.d1.value
+        instance.d2.pprint()
+        self.player_solution = instance.d2.value
